7acf366d1adcc5c0fe133dfc0c723c2e1f3abea3
kia57085 <kia57085@gmail.com>
2020-10-09 20:30:28 +0300

    Update lab1_1.py

--- lab1_1.py
+++ lab1_1.py
@@ -1,3 +1,2 @@
 array = ['g', 'h', 'a', 'q', 'a', 'k', 'g', 'h']
-print({arr: place_list for arr in array for place_list in
-    [[ind for ind, sym in enumerate(array) if sym == arr] for i in range(len(array))]})
+print({arr: [ind for ind, sym in enumerate(array) if sym == arr] for arr in array})
